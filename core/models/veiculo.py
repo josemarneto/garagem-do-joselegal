3feae01405cf26f5d92cc14cd3e8dcec4d288e5e
josemarneto <jc.mpneto@gmail.com>
2026-03-24 14:44:19 -0300

    feat: atualiza ForeignKey em Veiculo model para usar SET_NULL para modelo e cor

--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -4,8 +4,8 @@ from django.db import models
 class Veiculo(models.Model):
     ano = models.IntegerField(null=True, blank=True, default=0)
     preco = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True, default=0)
-    modelo = models.ForeignKey('Modelo', on_delete=models.CASCADE, null=True, blank=True)
-    cor = models.ForeignKey('Cor', on_delete=models.CASCADE, null=True, blank=True)
+    modelo = models.ForeignKey('Modelo', on_delete=models.SET_NULL, null=True, blank=True)
+    cor = models.ForeignKey('Cor', on_delete=models.SET_NULL, null=True, blank=True)
     acessorios = models.ManyToManyField('Acessorio', blank=True)
 
     def __str__(self):
