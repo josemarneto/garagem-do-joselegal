b2f6eb1764ee2bff56b41589d52fca7c70c7783a
josemarneto <jc.mpneto@gmail.com>
2026-03-24 14:41:34 -0300

    feat: remove ID from __str__ methods in Acessorio, Cor, and Modelo models

--- core/models/modelo.py
+++ core/models/modelo.py
@@ -7,4 +7,4 @@ class Modelo(models.Model):
     categorria = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
-        return f'({self.id}) - {self.nome}'
+        return self.nome
